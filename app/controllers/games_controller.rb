72c9c74dd2b7151f21a8398ae934454f2d565b70
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-10-12 17:32:06 +0100

    set up with new action and view started

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
+  def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def score
+  end
+end
